ca5cd1615d4357526c74e5f9efe2b9c78cae2729
Agustin978 <loboagus10@gmail.com>
2022-03-25 09:09:27 -0300

    Mi segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a = 12;
+int a = 25;
 int *point;
 
 int main()
 {
     point = &a;
-    printf("La direccion de memoria es: %p\n",point);
-    printf("El contenido al que apunta el puntero es: %d\n",*point);
-    printf("Espacio de memoria utilizado por el puntero es: %d\n", sizeof(point));
+    printf("El contenido al que apunta el puntero es: %d o %d\n",*point, a);
+    printf("La direccion de memoria almacenada por el puntero es: %d\n",point);
+    printf("La direccion de memoria almacenada por la variable es: %d\n", &a);
+    printf("Espacio de memoria utilizado por el puntero es: %d\n", sizeof(&a));
+    printf("La direccion de memoria del puntero es: %d\n", &point);
+    printf("El tamaño de memoria utilizado por el puntero es: %d\n", sizeof(&point));
     printf("Hola Mundo :v");
     return 0;
 }
